605ed04b3bffdad1e837217d0307ccf474436697
minh phan <21115053120333@sv.ute.udn.vn>
2024-04-06 18:26:29 +0700

    up1

--- src/components/OnlineShopRouter/configs/axiosClient.tsx
+++ src/components/OnlineShopRouter/configs/axiosClient.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "http://localhost:9000/",
+  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: "http://localhost:9000/",
   timeout: 30000,
 });
 
